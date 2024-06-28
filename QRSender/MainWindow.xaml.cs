6bdd0857e8c0d9b446a467c9051293a9ffa830e4
reggieqiao <reggieqiao@gmail.com>
2024-06-28 22:45:30 +0800

    feat: add CaptureScreen method

--- QRSender/MainWindow.xaml.cs
+++ QRSender/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,19 @@ namespace QRSender
 
         private void OnClick(object sender, RoutedEventArgs e)
         {
+            Bitmap screenshot = CaptureScreen();
+        }
 
+        private Bitmap CaptureScreen()
+        {
+            var screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            var screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+            Bitmap screenshot = new Bitmap(screenWidth, screenHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            using(Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, new System.Drawing.Size(screenWidth, screenHeight), CopyPixelOperation.SourceCopy);
+            }
+            return screenshot;
         }
     }
 }
